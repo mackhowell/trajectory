3c8755b75f1079b3f09a58a4edd9749f4b96d539
Mack <rmackhowell@gmail.com>
2014-04-24 02:12:56 -0400

    seethru View test test test

---
+++ src/com/example/trajectory/RepeatingAlarmServiceTester.java
@@ -0,0 +1,129 @@
+package com.example.trajectory;
+
+import java.io.IOException;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class RepeatingAlarmServiceTester extends BroadcastReceiver {
+	
+	private final static String LOG_TAG = "RepeatingAlarmServiceTester";
+	
+	private int imageWidth = 320;
+	private int imageHeight = 240;
+	private int frameRate = 30;
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.v(LOG_TAG, "HI FROM ALARM SERVICE TEST!");
+		Toast.makeText(context, "AALLLLARMMM!", Toast.LENGTH_LONG).show();
+	}
+	
+	class CameraSurface extends SurfaceView implements SurfaceHolder.Callback, PreviewCallback {
+		
+    	private boolean previewRunning = false;
+    	
+    	private SurfaceHolder holder;
+    	private Camera camera;
+    	
+    	private byte[] previewBuffer;
+    	
+    	long videoTimestamp = 0;
+    	
+    	Bitmap bitmap;
+    	Canvas canvas;
+
+		public CameraSurface(Context context) {
+			super(context);
+			
+    		holder = this.getHolder();
+    		holder.addCallback(this);
+    		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		}
+		
+		@Override
+		public void surfaceCreated(SurfaceHolder holder) {
+			
+    		camera = Camera.open();
+    		
+    		try {
+    			camera.setPreviewDisplay(holder);
+    			camera.setPreviewCallback(this);
+    			
+    			Camera.Parameters currentParams = camera.getParameters();
+    			Log.v(LOG_TAG,"Preview Framerate: " + currentParams.getPreviewFrameRate());
+	        	Log.v(LOG_TAG,"Preview imageWidth: " + currentParams.getPreviewSize().width + " imageHeight: " + currentParams.getPreviewSize().height);
+	        	
+	        	// Use these values
+	        	imageWidth = currentParams.getPreviewSize().width;
+	        	imageHeight = currentParams.getPreviewSize().height;
+	        	frameRate = currentParams.getPreviewFrameRate();
+	        	
+	        	bitmap = Bitmap.createBitmap(imageWidth, imageHeight, Bitmap.Config.ALPHA_8);
+	        	
+	        	/*
+				Log.v(LOG_TAG,"Creating previewBuffer size: " + imageWidth * imageHeight * ImageFormat.getBitsPerPixel(currentParams.getPreviewFormat())/8);
+	        	previewBuffer = new byte[imageWidth * imageHeight * ImageFormat.getBitsPerPixel(currentParams.getPreviewFormat())/8];
+				camera.addCallbackBuffer(previewBuffer);
+	            camera.setPreviewCallbackWithBuffer(this);
+	        	*/	
+	        	
+	        	camera.startPreview();
+	        	previewRunning = true;
+    		}
+    		catch (IOException e) {
+//    			Log.v(LOG_TAG, e.getMessage());
+    			e.printStackTrace();
+    		}
+			
+		}
+
+		@Override
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,int height) {
+			
+    		// get current parameters
+    		Camera.Parameters currentParams = camera.getParameters();
+    		Log.v(LOG_TAG, "Preview Framerate: " + currentParams.getPreviewFrameRate());
+    		Log.v(LOG_TAG, "Preview imageWidth: " + currentParams.getPreviewSize().width + " imageHeight: " + currentParams.getPreviewSize().height);
+    		
+    		// use these values
+    		imageWidth = currentParams.getPreviewSize().width;
+    		imageHeight = currentParams.getPreviewSize().height;
+    		frameRate = currentParams.getPreviewFrameRate();
+			
+		}
+
+		@Override
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			
+    		try {
+    			camera.setPreviewCallback(null);
+    			
+    			previewRunning = false;
+    			camera.release();
+    		}
+    		catch (RuntimeException e) {
+    			Log.v(LOG_TAG, e.getMessage());
+    			e.printStackTrace();
+    		}
+			
+		}
+
+		@Override
+		public void onPreviewFrame(byte[] data, Camera camera) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+}
--- src/com/example/trajectory/SensorService.java
+++ src/com/example/trajectory/SensorService.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -19,6 +21,8 @@ import android.app.PendingIntent;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -77,27 +81,12 @@ public class SensorService extends Service implements SensorEventListener {
 		return null;
 	}
 	
-	@Override
-	public void onDestroy() {
-		if (alarmManager != null) {
-			Intent intent = new Intent(this, RepeatingAlarmService.class);
-			alarmManager.cancel(PendingIntent.getBroadcast(this, REQUEST_CODE, intent, 0));
-		}
-		Toast.makeText(this, "Service Stopped!", Toast.LENGTH_SHORT).show();
-		Log.v(this.getClass().getName(), "Service onDestroy(). Stop AlarmManager at " + new java.sql.Timestamp(System.currentTimeMillis()).toString());
-		
-		// destroy alarm manager
-		((WindowManager)getSystemService(WINDOW_SERVICE)).removeView(myView);
-	    myView = null;
-	    
-	    // deregister LISTENER!
-	    sensorManager.unregisterListener(this);
-	}
-	
 	private void startService() {
 		
+		Log.v("SensorService","HI FROM startService()!!!");
+		
 		// Stuff for alarmManager
-		Intent intent = new Intent(this, RepeatingAlarmService.class);
+		Intent intent = new Intent(this, RepeatingAlarmServiceTester.class);
 		PendingIntent pendingIntent = PendingIntent.getBroadcast(this, REQUEST_CODE, intent, 0);
 		
 		// DO IT ONCE FOR TESTING STREAM...NOT REPEATING
@@ -107,10 +96,32 @@ public class SensorService extends Service implements SensorEventListener {
 				INTERVAL,
 				pendingIntent);
 		
+		Log.v("SensorService","JUST SET ALARM MANAGER!");
+		
 		Toast.makeText(this, "Service Started.", Toast.LENGTH_SHORT).show();
 		Log.v(this.getClass().getName(), "AlarmManager started at " + new java.sql.Timestamp(System.currentTimeMillis()).toString());
 	}
+	
+	@Override
+	public void onDestroy() {
+		if (alarmManager != null) {
+			Intent intent = new Intent(this, RepeatingAlarmServiceTester.class);
+			alarmManager.cancel(PendingIntent.getBroadcast(this, REQUEST_CODE, intent, 0));
+		}
+		Toast.makeText(this, "Service Stopped!", Toast.LENGTH_SHORT).show();
+		Log.v(this.getClass().getName(), "Service onDestroy(). Stop AlarmManager at " + new java.sql.Timestamp(System.currentTimeMillis()).toString());
+		
+		// destroy window manager
+		((WindowManager)getSystemService(WINDOW_SERVICE)).removeView(myView);
+	    myView = null;
+	    
+	    // deregister LISTENER!
+	    sensorManager.unregisterListener(this);
+	}
 
+	
+	
+	
 	
 	// HERE THEY ARE!
 	@Override
@@ -121,83 +132,91 @@ public class SensorService extends Service implements SensorEventListener {
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-    	Log.v("SENSOR CHANGED", "IM INSIDE SENSOR CHANGED FUNCTION!!!");
-		switch (event.sensor.getType()) {
-		case Sensor.TYPE_LINEAR_ACCELERATION:
-			Log.v("Accel","Z: " + event.values[2]);
-			if (event.values[2] >= 6)  {
-//				Toast toast = Toast.makeText(this, "YES IM HERE", Toast.LENGTH_SHORT);
-//				toast.show();
-			}
-			else {
-			}
-			break;
-		}
+
+//		switch (event.sensor.getType()) {
+//		case Sensor.TYPE_LINEAR_ACCELERATION:
+//			Log.v("Accel","Z: " + event.values[2]);
+//			if (event.values[2] >= 6)  {
+////				Toast toast = Toast.makeText(this, "YES IM HERE", Toast.LENGTH_SHORT);
+////				toast.show();
+//			}
+//			else {
+//			}
+//			break;
+//		}
+		
 //        ServicesDemo.refreshDisplay();
 		
 	}
 	
-
+	
+	
+	
+//public class MyLoadView extends SurfaceView implements SurfaceHolder.Callback, PreviewCallback {
+//
+//	public MyLoadView(Context context) {
+//		super(context);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	@Override
+//	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+//			int height) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void surfaceCreated(SurfaceHolder holder) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void surfaceDestroyed(SurfaceHolder holder) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void onPreviewFrame(byte[] data, Camera camera) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
+//}
+	
 	public class MyLoadView extends View {
-
-	    private Paint mPaint;
-
-	    public MyLoadView(Context context) {
-	        super(context);
-	        mPaint = new Paint();
-	        mPaint.setTextSize(50);
-	        mPaint.setARGB(200, 200, 200, 200);
-	    }
-
+		
+		private Paint mPaint;
+		
+		public MyLoadView(Context context) {
+			super(context);
+		    mPaint = new Paint();
+		    mPaint.setTextSize(50);
+		    mPaint.setARGB(200, 200, 200, 200);
+		}
+	
 	    @Override
 	    protected void onDraw(Canvas canvas) {
 	        super.onDraw(canvas);
 	        canvas.drawText("test test test", 0, 100, mPaint);
 	    }
-
+	
 	    @Override
 	    protected void onAttachedToWindow() {
 	        super.onAttachedToWindow();
 	    }
-
+	
 	    @Override
 	    protected void onDetachedFromWindow() {
 	        super.onDetachedFromWindow();
 	    }
-
+	
 	    @Override
 	    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 	        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 	    }
 	}
-	
-	
-	
-	
-	
-//	class MyListener implements SensorEventListener {
-//
-//		@Override
-//		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-//			// TODO Auto-generated method stub
-//			
-//		}
-//
-//		@Override
-//		public void onSensorChanged(SensorEvent event) {
-//			switch (event.sensor.getType()) {
-//			case Sensor.TYPE_LINEAR_ACCELERATION:
-////				Log.v("Accel","Z: " + event.values[2]);
-//				if (event.values[2] >= 6)  {
-////					Toast toast = Toast.makeText(this, "YES IM HERE", Toast.LENGTH_SHORT);
-////					toast.show();
-//				}
-//				else {
-//				}
-//				break;
-//			}
-//			
-//		}
-//	}
 
 }
